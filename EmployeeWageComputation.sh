ed0370b5e259d0f74813cec24b9c2eadb2bf04ff
sangeeta <sngtmth@gmail.com>
2021-09-07 00:35:10 +0530

    added to partTime Employee

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,14 +4,22 @@ echo "welcome to employee wage attendance system"
 
 #constant variable
 IS_PRESENT_full_time=1
+IS_PRESENT_part_time=2
 EMP_RATE_HR=20
-empCheck=$((RANDOM%2)) # 1 2
+empCheck=$((RANDOM%3)) # 1 2 3
 
 if [[ $empCheck -eq $IS_PRESENT_fulltime ]]
 then
         empHr=8
 
-else
+elif [[ $empCheck -eq $IS_PRESENT_part_time ]]
+then
+
         empHr=4
+else
+	empHr=0
 fi
-salary=$((empHr * EMp_RATE_HR ))
+
+salary=$(( empHr * EMp_RATE_HR ))
+
+
