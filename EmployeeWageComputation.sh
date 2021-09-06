e57baed5b87f9c0e2ae6ee2e1984b610c152585c
sangeeta <sngtmth@gmail.com>
2021-09-07 01:35:00 +0530

    function to get Work hours

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -9,25 +9,38 @@ EMP_RATE_HR=20
 
 MAX_WORKING_DAYS=20
 totalWorkHr=0
+MAX_WORK_HR=50
 day=1
-for (( day=1;day<=$MAX_WORKING_DAYS;day++ ))
-do
 
-#variable
-empCheck=$(( RANDOM%3 ))
+function getWorkHr(){
+        local empCheck=$1
+        local empHr=0
+        case $empCheck in
 
-case $empCheck in
-
-         $IS_PRESENT_full_time )
+              $IS_PRESENT_full_time )
                 empHr=8;;
-        $IS_PRESENT_part_time)
+                $IS_PRESENT_part_time)
                 empHr=4;;
         *)
                 empHr=0;;
 esac
+        echo $empHr
+
+}
+
+
+
+while [ $day -le $MAX_WORKING_DAYS -a $totalWorkHr -lt $MAX_WORK_HR ]
+do
+        #variable
+        empCheck=$(( RANDOM % 3 ))
+        empHr=$( getWorkHr $empCheck )
+        (( day++ ))
+
         totalWorkHr=$(( totalWorkHr + empHr ))
         salary=$(( empHr * EMP_RATE_HR ))
 done
+
 
 
 
