3cc806aa98d6f57b32f58c540db84c5c8cd6b198
sangeeta <sngtmth@gmail.com>
2021-09-06 18:42:01 +0530

    add EmployeeWageComputation

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,13 +4,14 @@ echo "welcome to employee wage attendance system"
 
 #constant variable
 IS_PRESENT_full_time=1
-
+EMP_RATE_HR=20
 empCheck=$((RANDOM%2)) # 1 2
 
 if [[ $empCheck -eq $IS_PRESENT_fulltime ]]
 then
-        echo "Emp present"
+        empHr=8
 
 else
-        echo "Emp Absent"
+        empHr=4
 fi
+salary=$((empHr * EMp_RATE_HR ))
