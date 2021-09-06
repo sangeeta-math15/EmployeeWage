47d49712db51a2becc675403401a6706adf18b2a
sangeeta <sngtmth@gmail.com>
2021-09-07 00:52:09 +0530

    added to switchCase

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,14 +4,19 @@ echo "welcome to employee wage attendance system"
 
 #constant variable
 IS_PRESENT_full_time=1
+IS_PRESENT_part_time=2
 EMP_RATE_HR=20
 empCheck=$((RANDOM%2)) # 1 2
 
-if [[ $empCheck -eq $IS_PRESENT_fulltime ]]
-then
-        empHr=8
+case $empCheck in
 
-else
-        empHr=4
-fi
-salary=$((empHr * EMp_RATE_HR ))
+         $IS_PRESENT_full_time )
+                empHr=8;;
+        $IS_PRESENT_part_time)
+                empHr=4;;
+        *)
+                empHr=0;;
+esac
+
+
+salary=$((empHr * EMP_RATE_HR ))
